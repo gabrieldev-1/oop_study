71fd00102a8fa0adc2dbd7ae9de2a5cde3ad1946
Gabriel dos Santos <developergabrieldossantos@gmail.com>
2025-07-31 21:54:07 -0300

    '/library' exercise(Book class)

---
+++ exercices/library/Book.java
@@ -0,0 +1,34 @@
+package library;
+
+public class Book {
+    // Atibutos;
+    private String name;
+    private int numberOfPages;
+    private boolean status;
+
+    public Book(String name, int numberOfPages, boolean status) {
+        this.name = name;
+        this.numberOfPages = numberOfPages;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+
+    }
+    public String getStatus() {
+        return status ? "Disponivel" : "Indisponivel";
+    }
+
+    public void showInfos() {
+        System.out.println("------ Informacoes do livro ------");
+        System.out.println("Nome: " + this.getName());
+        System.out.println("Numero de paginas: " + this.getNumberOfPages());
+        System.out.println("Status do livro: " + this.getStatus());
+        System.out.println("----------------------------------");
+    }
+}
---
+++ exercices/library/Library.java
@@ -0,0 +1,9 @@
+package library;
+
+public class Library {
+    public static void main(String[] args) {
+        Book livro01 = new Book("Crime e Castigo", 500, true);
+
+        livro01.showInfos();
+    }
+}
